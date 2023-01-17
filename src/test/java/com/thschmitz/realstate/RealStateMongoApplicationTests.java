22646689b2bd4c8a1bb5b7372b2ebb6a47a2c0e7
thschmitz <thomas.henrique.schmitz@gmail.com>
2023-01-17 13:47:16 -0300

    Setup

---
+++ src/test/java/com/thschmitz/realstate/RealStateMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thschmitz.realstate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealStateMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

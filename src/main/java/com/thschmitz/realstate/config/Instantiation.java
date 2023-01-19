17618774d153853e570db5950c8082790b1272e3
thschmitz <thomas.henrique.schmitz@gmail.com>
2023-01-19 11:33:26 -0300

    Comments in Posts

--- src/main/java/com/thschmitz/realstate/config/Instantiation.java
+++ src/main/java/com/thschmitz/realstate/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.thschmitz.realstate.domain.Post;
 import com.thschmitz.realstate.domain.User;
 import com.thschmitz.realstate.dto.AuthorDTO;
+import com.thschmitz.realstate.dto.CommentDTO;
 import com.thschmitz.realstate.repository.PostRepository;
 import com.thschmitz.realstate.repository.UserRepository;
 
@@ -43,6 +44,13 @@ public class Instantiation implements CommandLineRunner{
 		Post post2 = new Post(null, sdf.parse("23/10/2022"), "Teste02", "Testando um novo post2", "imagemNenhuma", 20, "A venda", new AuthorDTO(alex));
 		Post post3 = new Post(null, sdf.parse("05/09/2022"), "Teste03", "Testando um novo post3", "imagemNenhuma", 30, "A venda", new AuthorDTO(bob));
 		
+		CommentDTO c1 = new CommentDTO("Post1Comment", sdf.parse("21/05/2019"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Post2Comment", sdf.parse("19/07/2020"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Post3Comment", sdf.parse("11/02/2021"), new AuthorDTO(maria));
+		
+		post1.getComments().addAll(Arrays.asList(c1));
+		post2.getComments().addAll(Arrays.asList(c2));
+		post3.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2, post3));
 		
--- src/main/java/com/thschmitz/realstate/domain/Post.java
+++ src/main/java/com/thschmitz/realstate/domain/Post.java
@@ -1,13 +1,16 @@
 package com.thschmitz.realstate.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.thschmitz.realstate.dto.AuthorDTO;
+import com.thschmitz.realstate.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -22,6 +25,8 @@ public class Post implements Serializable {
 	private Integer likes;
 	private String status;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
@@ -102,6 +107,14 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -119,6 +132,4 @@ public class Post implements Serializable {
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
-
-
 }

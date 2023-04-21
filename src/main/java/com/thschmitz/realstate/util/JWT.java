3ba9adc5e381d0d2861edf7d9d01713f9bed858d
Thomas Schmitz <thomas.henrique.schmitz@gmail.com>
2023-04-21 16:23:38 -0300

    JUnit Tests JWT

--- src/main/java/com/thschmitz/realstate/util/JWT.java
+++ src/main/java/com/thschmitz/realstate/util/JWT.java
@@ -10,6 +10,7 @@ import javax.crypto.spec.SecretKeySpec;
 import com.thschmitz.realstate.domain.Users;
 import com.thschmitz.realstate.exception.ExpiredJwtException;
 import com.thschmitz.realstate.exception.InvalidJWT;
+import com.thschmitz.realstate.exception.ParseException;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import io.jsonwebtoken.Claims;
@@ -19,30 +20,35 @@ import io.jsonwebtoken.SignatureAlgorithm;
 
 public class JWT {
 	public static String createJWT(Users newObj) {
-		Dotenv dotenv = Dotenv.configure().directory("./.env").load();
-		
-		String secret = dotenv.get("secret_JWT");
-		Key hmacKey = new SecretKeySpec(Base64.getDecoder().decode(secret), 
-                SignatureAlgorithm.HS256.getJcaName());
-		String role;
-		
-		if(newObj.getAdmin()) {
-			role = "Admin";
-		} else {
-			role = "User";
+		try {
+			Dotenv dotenv = Dotenv.configure().directory("./.env").load();
+			
+			String secret = dotenv.get("secret_JWT");
+			Key hmacKey = new SecretKeySpec(Base64.getDecoder().decode(secret), 
+	                SignatureAlgorithm.HS256.getJcaName());
+			String role;
+			
+			if(newObj.getAdmin()) {
+				role = "Admin";
+			} else {
+				role = "User";
+			}
+			
+			Instant now = Instant.now();
+			String jwtToken = Jwts.builder()
+			        .claim("id", newObj.getId())
+			        .setSubject(newObj.getName())
+			        .setSubject(role)
+			        .setIssuedAt(Date.from(now))
+			        .setExpiration(Date.from(now.plus(10l, ChronoUnit.MINUTES)))
+			        .signWith(hmacKey)
+			        .compact();
+			
+			return jwtToken;	
+		} catch(NullPointerException e) {
+			throw new NullPointerException(null);
 		}
 		
-		Instant now = Instant.now();
-		String jwtToken = Jwts.builder()
-		        .claim("id", newObj.getId())
-		        .setSubject(newObj.getName())
-		        .setSubject(role)
-		        .setIssuedAt(Date.from(now))
-		        .setExpiration(Date.from(now.plus(10l, ChronoUnit.MINUTES)))
-		        .signWith(hmacKey)
-		        .compact();
-		
-		return jwtToken;
 	}
 	
 	public static Jws<Claims> validateJWT(String jwt) {
---
+++ src/test/java/com/thschmitz/realstate/JWTTest.java
@@ -0,0 +1,40 @@
+package com.thschmitz.realstate;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.thschmitz.realstate.domain.Users;
+import com.thschmitz.realstate.exception.ParseException;
+import com.thschmitz.realstate.util.JWT;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+public class JWTTest {
+
+	@Test
+	void generateSuccessJWTForMaria() {
+		var maria = new Users();
+		
+		maria.setId(244);
+		maria.setName("Maria Brown");
+		maria.setAdmin(true);
+		
+		assertDoesNotThrow(() -> {JWT.createJWT(maria);});
+	}
+	
+	@Test
+	void generateErrorJWTForMaria() {
+		var maria = new Users();
+		
+		maria.setName("Maria Brown");
+		
+		assertThrows(NullPointerException.class, () -> {JWT.createJWT(maria);});
+	}
+}
